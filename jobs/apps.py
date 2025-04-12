98af591a5225be2adca6d0d37c17f1a049bfef6b
adarshh12 <132646093+adarshh12@users.noreply.github.com>
2025-04-12 16:06:00 +0530

    Add files via upload

---
+++ jobs/apps.py
@@ -0,0 +1,8 @@
+"""URL configuration for the jobs app."""
+
+from django.apps import AppConfig
+
+# App config
+class JobsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jobs'
